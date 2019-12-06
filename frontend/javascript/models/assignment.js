055a56216d5228c0025cf90b92bff063eea92974
github username <github email address>
2019-12-06 13:18:09 -0500

    load models js

---
+++ frontend/javascript/models/assignment.js
@@ -0,0 +1,4 @@
+document.addEventListener('DOMContentLoaded',
+() => {
+  console.log('assignment.js is loaded...')
+});
---
+++ frontend/javascript/models/subject.js
@@ -0,0 +1,4 @@
+document.addEventListener('DOMContentLoaded',
+() => {
+  console.log('subject.js is loaded...')
+});
