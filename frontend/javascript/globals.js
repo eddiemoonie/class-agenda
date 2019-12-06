315e1fb9878e2b4d2df5afe608ae6b529326eebb
github username <github email address>
2019-12-06 16:35:40 -0500

    fetch

--- frontend/javascript/globals.js
+++ frontend/javascript/globals.js
@@ -3,6 +3,8 @@ document.addEventListener('DOMContentLoaded',
   console.log('globals.js is loaded...')
 });
 
+const parseJSON = response => response.json()
+
 const renderFormBtn = document.getElementById('render-form-button')
 const subjectForm = document.getElementById('subject-form-container')
 const addSubjectBtn = document.getElementById('add-subject-button')
